7ea264f8c71d30a9918afb81983fcfb43813c088
AbhiDilliwal <mail@dilliwal.com>
2013-04-23 14:31:38 +0530

    Modified some templates, for testing.

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -4,7 +4,7 @@ angular.module('<%= _.camelize(appname) %>App')
   .controller('<%= _.classify(name) %>Ctrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
-      'AngularJS',
+      'MyAngularJS',
       'Karma'
     ];
   });
